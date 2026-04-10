0acf24b7f007d116417d790ae1e5eee984ca97d8
gabrielnegocio03-debug <gabrielnegocio03@gmail.com>
2026-04-09 22:41:17 -0300

    Add CORS middleware to server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const path    = require('path');
 const QRCode  = require('qrcode');
 const pino    = require('pino');
@@ -6,6 +7,8 @@ const { makeWASocket, DisconnectReason, useMultiFileAuthState, fetchLatestBailey
 const { Boom } = require('@hapi/boom');
 
 const app = express();
+
+app.use(cors()); // 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
